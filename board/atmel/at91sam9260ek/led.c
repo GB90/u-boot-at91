225d080a393bcaedf663d5cd8a6401ada8bd9f68
GB90 <guobing116633@163.com>
2020-07-12 14:38:47 +0800

    Update led.c
    根据V3B的原理图修改

--- board/atmel/at91sam9260ek/led.c
+++ board/atmel/at91sam9260ek/led.c
@@ -13,9 +13,13 @@
 void coloured_LED_init(void)
 {
 	/* Clock is enabled in board_early_init_f() */
-	at91_set_gpio_output(CONFIG_RED_LED, 1);
-	at91_set_gpio_output(CONFIG_GREEN_LED, 1);
+	at91_set_gpio_output(CONFIG_GREEN_LED1, 1);
+	at91_set_gpio_output(CONFIG_GREEN_LED2, 1);
+	at91_set_gpio_output(CONFIG_GREEN_LED3, 1);
+	at91_set_gpio_output(CONFIG_GREEN_LED4, 1);
 
-	at91_set_gpio_value(CONFIG_RED_LED, 0);
-	at91_set_gpio_value(CONFIG_GREEN_LED, 1);
+	at91_set_gpio_value(CONFIG_GREEN_LED1, 0);
+	at91_set_gpio_value(CONFIG_GREEN_LED2, 0);
+	at91_set_gpio_value(CONFIG_GREEN_LED3, 0);
+	at91_set_gpio_value(CONFIG_GREEN_LED4, 0);
 }
